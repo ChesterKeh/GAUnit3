90bac9373696568e774b150b0090a06f85db5744
Chester Keh <rpchester18@gmail.com>
2024-01-12 13:42:05 +0800

    Add dotenv and mongoose dependencies, update server.js formatting

--- MERN-Stack/server.js
+++ MERN-Stack/server.js
@@ -1,15 +1,15 @@
-//*Require block
+//* require block
+require("dotenv").config();
+require("./config/database");
 const express = require("express");
 const path = require("path");
-const favicon = require("serve-favicon");
 const logger = require("morgan");
 
 const app = express();
 
-//* Middleware block
+//* middleware block
 app.use(logger("dev"));
 app.use(express.json());
-// app.use(favicon(path.join(__dirname, "dist", "favicon.ico")));
 app.use(express.static(path.join(__dirname, "dist")));
 
 //* routes block
@@ -18,7 +18,6 @@ app.get("/*", function (req, res) {
 });
 
 //* listen block
-
 const port = process.env.PORT || 3001;
 
 app.listen(port, function () {
