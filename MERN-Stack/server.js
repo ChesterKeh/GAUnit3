9e09d18d9de5e87d7350c9cdcad959514c405ad1
Chester Keh <rpchester18@gmail.com>
2024-01-12 13:59:11 +0800

    Update server.js and vite.config.js

--- MERN-Stack/server.js
+++ MERN-Stack/server.js
@@ -18,7 +18,7 @@ app.get("/*", function (req, res) {
 });
 
 //* listen block
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
   console.log(`Express app running on port ${port}`);
